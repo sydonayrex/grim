c39990b2e648e53f3cfe0916b96a22334b754191
sydonayrex <123185209+sydonayrex@users.noreply.github.com>
2026-08-17 12:23:33 -0500

    Delete extract_tuna1_gap.py

--- extract_tuna1_gap.py
+++
@@ -1,6 +0,0 @@
-#!/usr/bin/env python3
-"""Extract 2 new PDFs + 1 new HTML from old/tuna1/ that weren't processed last run."""
-import os, subprocess
-
-TUNA1 = "/D/rex/projects/grim/old/tuna1"
-OUTDIR = "/D/rex/projects/grim/old/tuna1_t...[truncated]
